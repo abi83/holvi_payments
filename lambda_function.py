6d3a8c78a477fdac10b2c96587026909f98e9d71
abi83 <vladimir.kromm@yandex.ru>
2021-07-30 20:14:59 +0200

    workflow works

--- lambda_function.py
+++ lambda_function.py
@@ -8,12 +8,12 @@ def lambda_handler(event, context):
         base64_message = event.get('body')
         base64_bytes = base64_message.encode('ascii')
         message_bytes = base64.b64decode(base64_bytes)
-        # message = message_bytes.decode('ascii')
+        message = message_bytes.decode('ascii')
     except AttributeError:
-        pass
+        message = 'Hello from NEW Lambda!'
 
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from NEW Lambda!')
+        'body': json.dumps(message)
     }
