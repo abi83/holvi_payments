0029309adf919281f696b802b12c2737128651d0
abi83 <vladimir.kromm@yandex.ru>
2021-07-30 20:12:31 +0200

    workflow 8 test

--- lambda_function.py
+++ lambda_function.py
@@ -4,15 +4,16 @@ import base64
 
 
 def lambda_handler(event, context):
-    base64_message = event.get('body')
-    base64_bytes = base64_message.encode('ascii')
-    message_bytes = base64.b64decode(base64_bytes)
-    message = message_bytes.decode('ascii')
-
-    print('POST BODY ENCODED', json.dumps(message))
+    try:
+        base64_message = event.get('body')
+        base64_bytes = base64_message.encode('ascii')
+        message_bytes = base64.b64decode(base64_bytes)
+        message = message_bytes.decode('ascii')
+    except AttributeError:
+        pass
 
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Hello from NEW Lambda!')
     }
