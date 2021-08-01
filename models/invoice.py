56617f9281fc6c37b7916be304679ede51748cb5
abi83 <vladimir.kromm@yandex.ru>
2021-08-01 15:42:12 +0200

    some more workflow

--- models/invoice.py
+++ models/invoice.py
@@ -1,64 +1,81 @@
-from .payent import Payment
 from .incoice_line import InvoiceLine
+from .payent import Payment
 
 
 class Invoice:
     def __init__(self, invoice_lines, payments):
         self.invoice_lines = [InvoiceLine(**line) for line in invoice_lines]
         self.payments = [Payment(**payment) for payment in payments]
-
-    def to_JSON(self):
-        return self.categories_weight()
+        self._invoice_sum_cached = None
+        self._categories_proportions = None
 
     @property
     def invoice_sum(self):
-        # TODO: check if invoice lines summ is not equal to payments sum
-        return sum([
-            line.unit_price_net * line.quantity for line in self.invoice_lines
-        ])
+        if self._invoice_sum_cached is None:
+            self._invoice_sum_cached = round(sum([
+                line.unit_price_net * line.quantity for line in self.invoice_lines
+            ]), 2)
+        return self._invoice_sum_cached
 
-    def categories_weight(self):
-        x = {}
-        for element in self.invoice_lines:
-            try:
-                net_amount = element.unit_price_net * element.quantity
-                x[element.category] += net_amount
-            except KeyError:
-                x[element.category] = element.unit_price_net * element.quantity
-        return {key: value / self.invoice_sum for key, value in x.items()}
+    @property
+    def categories_proportions(self):
+        if self._categories_proportions is None:
+            categories_sum = {}
+            for element in self.invoice_lines:
+                try:
+                    categories_sum[element.category] += element.unit_price_net * element.quantity
+                except KeyError:
+                    categories_sum[element.category] = element.unit_price_net * element.quantity
+            self._categories_proportions = {key: value/self.invoice_sum for key, value in categories_sum.items()}
+        return self._categories_proportions
+
+    @staticmethod
+    def fix_mistake(payment_obj, mistake):
+        fixes_number = abs(int(mistake / 0.01))
+        fixes_value = round(mistake / fixes_number, 2)
+        max_categories = [{'index': 0, 'net_amount': payment_obj['categorisations'][0]['net_amount']}]
+        for index, item in enumerate(payment_obj['categorisations']):
+            if item['net_amount'] > max([el['net_amount'] for el in max_categories]):
+                max_categories.append({'index': index, 'net_amount': item['net_amount']})
+                max_categories.sort(key=lambda item: item['net_amount'])
+                max_categories.pop(0)
+        for index in [el['index'] for el in max_categories]:
+            payment_obj['categorisations'][index]['net_amount'] = round(
+                payment_obj['categorisations'][index]['net_amount'] - fixes_value, 2)
+        return payment_obj
+
+    @staticmethod
+    def calculate_mistake(payment_obj, right_value):
+        return round(sum([category['net_amount'] for category in payment_obj['categorisations']]) - right_value, 2)
 
     def payments_categorisations(self):
-        x = [
-            {
+        categorisation = []
+        for payment in self.payments:
+            categorized_payment = {
                 'id': payment.id,
                 'categorisations': [
                     {
                         'category': key,
-                        'net_amount': str(round(payment.amount * value, 2))
+                        'net_amount': round(payment.amount * value, 2)
                     }
-                    for key, value in self.categories_weight().items()
-                ]
+                    for key, value in self.categories_proportions.items()
+                ],
             }
-            for payment in self.payments
-        ]
-        invoice_payment_sum, cat_payment_amount = 0, 0
-        for payment in self.payments:
-            for i in self.payments:
-                if i.id == payment.id:
-                    invoice_payment_sum = i.amount
-            for index, value in enumerate(x):
-                if value['id'] == payment.id:
-                    payment_index = index
-            cat_payment_amount = sum([
-                float(item['net_amount'])
-                for item in x[payment_index]['categorisations']])
-            if invoice_payment_sum != cat_payment_amount:
-                dif = round(invoice_payment_sum - cat_payment_amount, 2)
-                if abs(dif) >= 0.01:
-                    raise ValueError(f'Rounding error, diff: {dif},'
-                                     f'lines: {len(self.invoice_lines)},'
-                                     f'paymnts: {len(self.payments)}')
-                else:
-                    print(f'Very small dif'
-                          f'{invoice_payment_sum - cat_payment_amount}')
-        return x
+            mistake = self.calculate_mistake(categorized_payment, payment.amount)
+            if abs(mistake) >= 0.01:
+                mistake_after_fix = mistake
+                while abs(mistake_after_fix) >= 0.01:
+                    categorized_payment = self.fix_mistake(categorized_payment, mistake_after_fix)
+                    mistake_after_fix = self.calculate_mistake(categorized_payment, payment.amount)
+            self.validate_output(categorized_payment, payment.amount)
+            categorisation.append(categorized_payment)
+        return categorisation
+
+    def validate_output(self, data, right_value):
+        mistake = self.calculate_mistake(data, right_value)
+        if abs(mistake) >= 0.01:
+            raise ValueError(f'Rounding error, '
+                             f'diff: {round(mistake, 2)}, '
+                             f'lines: {len(self.invoice_lines)}, '
+                             f'paymnts: {len(self.payments)} '
+                             )
--- tests/tests.py
+++ tests/tests.py
@@ -1,19 +1,27 @@
 import random
+from datetime import datetime, timedelta
 from faker import Faker
 from models import Invoice
 
 
-def test_case_fake():
-    assert 5 == 5
+def timer(func):
+    def timed(*args, **kw):
+        ts = datetime.now()
+        print('Started:', ts.strftime("%H:%M:%S  %f"))
+        result = func(*args, **kw)
+        te = datetime.now()
+        print('Ended:', te.strftime("%H:%M:%S %f"))
+        execution_time = te-ts
+        print('Execution time:', execution_time.microseconds, 'microseconds')
+        return result
+    return timed
 
-
-def test_case2_fake():
-    assert 6 == 6
-
-
+@timer
 def my_tester(number):
-    MAX_CATEGORIES_COUNT = 5
-    MAX_PAYMENTS_COUNT = 3
+    MAX_CATEGORIES_COUNT = 100
+    MAX_PAYMENTS_COUNT = 300
+    MAX_LINES_COUNT = MAX_CATEGORIES_COUNT * 7
+    MAX_PRICE = 10_000_000
 
     for test_case_number in range(number):
         fake = Faker()
@@ -24,41 +32,47 @@ def my_tester(number):
             {
                 "description": fake.catch_phrase(),
                 "quantity": fake.random_int(min=1, max=100),
-                "category": categories[random.randint(0, len(categories) - 1)],
+                "category": categories[
+                    random.randint(0, len(categories) - 1)],
                 "unit_price_net": fake.pyfloat(right_digits=2,
-                                               positive=True, max_value=100)
-            } for _ in range(random.randint(1, MAX_CATEGORIES_COUNT * 3))
+                                               positive=True,
+                                               max_value=MAX_PRICE)
+            } for _ in range(random.randint(1, MAX_LINES_COUNT))
         ]
-        invoice_sum = sum(line['unit_price_net'] * line['quantity']
-                          for line in invoice_lines)
+        invoice_lines_sum = round(
+            sum(line['unit_price_net'] * line['quantity'] for line in
+                invoice_lines),
+            2)
         payment_number = random.randint(1, MAX_PAYMENTS_COUNT)
         payments = []
+        sum_left = invoice_lines_sum
         for index in range(payment_number):
-            payment_amount = fake.pyfloat(
-                right_digits=2, positive=True,
-                min_value=int(invoice_sum / (4 * payment_number)) + 1,
-                max_value=int(invoice_sum))
-            # print('payment_amount', payment_amount)
-            if invoice_sum > payment_amount:
-                invoice_sum -= payment_amount
+            payment_sum = fake.pyfloat(right_digits=2, positive=True,
+                                       min_value=int(invoice_lines_sum / (
+                                                   4.5 * payment_number)) + 1,
+                                       max_value=int(
+                                           invoice_lines_sum / 3))
+            if sum_left > payment_sum:
+                sum_left = round(sum_left - payment_sum, 2)
             else:
-                payment_amount = round(invoice_sum, 2)
+                payment_sum = round(sum_left, 2)
+            assert payment_sum >= 0 and sum_left > 0
             payments.append(
                 {
                     "id": index,
-                    "amount": payment_amount
+                    "amount": payment_sum
                 }
             )
         x = Invoice(invoice_lines, payments)
         try:
             x.payments_categorisations()
-            print('Successs')
+            # print('Successs')
         except ValueError as e:
             print(e)
 
 
 def test_runner():
-    my_tester(100)
+    my_tester(500)
 
 
 if __name__ == '__main__':
