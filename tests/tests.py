38b558ded4c5b47cfd4ba11331766f7ea3b4bec8
abi83 <vladimir.kromm@yandex.ru>
2021-08-02 01:39:25 +0200

    no paralel run

--- tests/tests.py
+++ tests/tests.py
@@ -3,23 +3,23 @@ from timer import timer
 
 
 def test_xs():
-    invoice_categorisation_tester(1000, ranges='xs')
+    invoice_categorisation_tester(10, ranges='xs')
 
 
 def test_s():
-    invoice_categorisation_tester(1000, ranges='s')
+    invoice_categorisation_tester(10, ranges='s')
 
 
 def test_m():
-    invoice_categorisation_tester(1000, ranges='m')
+    invoice_categorisation_tester(10, ranges='m')
 
 
 def test_l():
-    invoice_categorisation_tester(500, ranges='l')
+    invoice_categorisation_tester(5, ranges='l')
 
 
 def test_xl():
-    invoice_categorisation_tester(100, ranges='xl')
+    invoice_categorisation_tester(4, ranges='xl')
 
 
 @timer
