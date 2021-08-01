1eb85b6e8cc51bf92c6b296fea8c010b8c990ded
abi83 <vladimir.kromm@yandex.ru>
2021-08-01 16:14:22 +0200

    tester function refactored

--- models/payent.py
+++ models/payent.py
@@ -1,4 +1,31 @@
+# from .money import Money
+
 class Payment:
     def __init__(self, id, amount):
         self.id = id
         self.amount = amount
+        self.categorisations = []
+
+    def calc_mistake(self):
+        return round(sum(category['net_amount'] for category in self.categorisations) - self.amount, 2)
+
+    def categorise_payment(self, cat_proportions: dict, categories_limits: dict):
+        money_left = self.amount
+        for category, proportion in cat_proportions.items():
+            net_amount = round(
+                        min(
+                            self.amount * proportion,
+                            categories_limits[category],
+                            money_left
+                        ), 2)
+            self.categorisations.append(
+                {
+                    'category': category,
+                    'net_amount': net_amount
+                })
+            money_left -= net_amount
+        return {
+            category['category']: round(categories_limits[category['category']] - category['net_amount'], 2)
+            for category in self.categorisations
+        }
+
--- tests/tests.py
+++ tests/tests.py
@@ -1,80 +1,34 @@
-import random
-from datetime import datetime
-from faker import Faker
-from models import Invoice
+from invoice_tester import invoice_categorisation_tester
+from timer import timer
+
+def test_xs():
+    invoice_categorisation_tester(1000, ranges='xs')
 
 
-def timer(func):
-    def timed(*args, **kw):
-        ts = datetime.now()
-        print('Started:', ts.strftime("%H:%M:%S  %f"))
-        result = func(*args, **kw)
-        te = datetime.now()
-        print('Ended:', te.strftime("%H:%M:%S %f"))
-        execution_time = te - ts
-        print('Execution time:', execution_time.microseconds, 'microseconds')
-        return result
-    return timed
+def test_s():
+    invoice_categorisation_tester(1000, ranges='s')
+
+
+def test_m():
+    invoice_categorisation_tester(1000, ranges='m')
+
+
+def test_l():
+    invoice_categorisation_tester(500, ranges='l')
+
+
+def test_xl():
+    invoice_categorisation_tester(100, ranges='xl')
 
 
 @timer
-def my_tester(number):
-    MAX_CATEGORIES_COUNT = 100
-    MAX_PAYMENTS_COUNT = 300
-    MAX_LINES_COUNT = MAX_CATEGORIES_COUNT * 7
-    MAX_PRICE = 10_000_000
-
-    for test_case_number in range(number):
-        fake = Faker()
-
-        categories_number = random.randint(1, MAX_CATEGORIES_COUNT)
-        categories = [fake.bs() for _ in range(categories_number)]
-        invoice_lines = [
-            {
-                "description": fake.catch_phrase(),
-                "quantity": fake.random_int(min=1, max=100),
-                "category": categories[
-                    random.randint(0, len(categories) - 1)],
-                "unit_price_net": fake.pyfloat(right_digits=2,
-                                               positive=True,
-                                               max_value=MAX_PRICE)
-            } for _ in range(random.randint(1, MAX_LINES_COUNT))
-        ]
-        invoice_lines_sum = round(
-            sum(line['unit_price_net'] * line['quantity'] for line in
-                invoice_lines),
-            2)
-        payment_number = random.randint(1, MAX_PAYMENTS_COUNT)
-        payments = []
-        sum_left = invoice_lines_sum
-        for index in range(payment_number):
-            payment_sum = fake.pyfloat(
-                right_digits=2,
-                positive=True,
-                min_value=int(invoice_lines_sum / (4.5 * payment_number)) + 1,
-                max_value=int(invoice_lines_sum / 3))
-            if sum_left > payment_sum:
-                sum_left = round(sum_left - payment_sum, 2)
-            else:
-                payment_sum = round(sum_left, 2)
-            assert payment_sum >= 0 and sum_left > 0
-            payments.append(
-                {
-                    "id": index,
-                    "amount": payment_sum
-                }
-            )
-        x = Invoice(invoice_lines, payments)
-        try:
-            x.payments_categorisations()
-            # print('Successs')
-        except ValueError as e:
-            print(e)
-
-
-def test_runner():
-    my_tester(500)
+def main():
+    invoice_categorisation_tester(1000, ranges='xs')
+    invoice_categorisation_tester(1000, ranges='s')
+    invoice_categorisation_tester(1000, ranges='m')
+    invoice_categorisation_tester(500, ranges='l')
+    invoice_categorisation_tester(100, ranges='xl')
 
 
 if __name__ == '__main__':
-    test_runner()
+    main()
